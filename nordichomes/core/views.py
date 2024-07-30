6a2e973d23377820b4513a2e2227637afcf03d1e
Vansh Srivastav <vanshrivastav2002@gmaile.com>
2024-07-30 15:01:41 +0530

    Create Shope & Product Page

--- nordichomes/core/views.py
+++ nordichomes/core/views.py
@@ -1,4 +1,33 @@
+from django.db.models import Q
 from django.shortcuts import render
 
+from products.models import Product, Category
+
 def frontpage(request):
-    return render(request, 'core/frontpage.html')
+    products = Product.objects.all()[0:8]
+    return render(request, 'core/frontpage.html', {'products': products})
+
+def shop(request):
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    active_category = request.GET.get('category',  '')
+
+    if active_category:
+        products = products.filter(category__slug=active_category)
+
+    query = request.GET.get('query', '')
+
+    if query:
+        products = products.filter(name__icontains=query)
+
+    context = {
+        'categories': categories,
+        'products': products,
+        'active_category': active_category
+    }
+
+    return render(request, 'core/shop.html', context)
+
+
+
